700c3a2782fb797c240f7bd0f72c329ac875ec79
Jason Dunkelberger (dirkraft) <dirkraft@gmail.com>
2013-12-09 11:17:29 -0600

    Add Option-like alternative to try-catch'ing ElasticSearch induced JresErrorResponseException's.

--- src/main/java/com/blacklocus/jres/Jres.java
+++ src/main/java/com/blacklocus/jres/Jres.java
@@ -4,10 +4,12 @@ import com.blacklocus.jres.http.HttpClientFactory;
 import com.blacklocus.jres.http.HttpMethods;
 import com.blacklocus.jres.request.JresRequest;
 import com.blacklocus.jres.response.JresResponse;
+import com.blacklocus.jres.response.common.JresErrorResponseException;
 import com.blacklocus.jres.strings.JresPaths;
 import com.google.common.base.Supplier;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.codehaus.jackson.JsonNode;
 
 import java.io.IOException;
 
@@ -28,11 +30,10 @@ public class Jres {
     }
 
     /**
-     *
      * @param <Q> request - type of request object
      * @param <R> response - type of response object produced as the returned value
      * @param <B> basis - basic type of value encapsulated by the response. Think of it as a shallow interpretation
-     *           directly of the ElasticSear
+     *            directly of the ElasticSearch response body.
      * @return corresponding response object ({@link JresRequest}'s RESPONSE type)
      */
     public <B, R extends JresResponse<B>, Q extends JresRequest<B, R>> R request(Q request) {
@@ -49,5 +50,81 @@ public class Jres {
 
     }
 
+    /**
+     * Like {@link #request(JresRequest)} but wraps in a try-catch for {@link JresErrorResponseException} and returns
+     * it gracefully if the status code matches that given. This is sometimes preferable to inline try-catch blocks for
+     * expected error responses, but otherwise functionally equivalent -- e.g. checking if an index exists will return
+     * a 404 which is normally translated into a thrown JresErrorResponseException. If 404 was given as the
+     * tolerated status then such a JresErrorResponseException will be wrapped up into a {@link Tolerance}.
+     * <p/>
+     * Currently assumes a JsonNode basis for now, to keep exposed signatures' generics simpler.
+     *
+     * @param request         JresRequest with JsonNode basis
+     * @param toleratedStatus which if encountered will return wrapped up in a Tolerance instead of being thrown
+     * @param <Q>             request - type of request object
+     * @param <R>             response - type of response object produced as the returned value
+     * @return a Tolerance which wraps up the possible ok response or error response. Check {@link Tolerance#isError()}.
+     * @see Tolerance
+     */
+    public <R extends JresResponse<JsonNode>, Q extends JresRequest<JsonNode, R>> Tolerance<R> tolerate(Q request, int toleratedStatus) {
+
+        try {
+            return new Tolerance<R>(false, request(request));
+        } catch (JresErrorResponseException e) {
+            if (e.getStatus() != toleratedStatus) {
+                throw e;
+            } else {
+                return new Tolerance<R>(true, e);
+            }
+        }
+
+    }
+
+    /**
+     * @param <R> response - type of response object produced for a successful ElasticSearch response. Currently assumes
+     *            a JsonNode basis.
+     */
+    public static class Tolerance<R extends JresResponse<JsonNode>> {
+        // Assume JsonNode basis for now, keep exposed signatures' generics simpler.
+
+        private final boolean error;
+        private final JresErrorResponseException exception;
+        private final R response;
+
+        Tolerance(boolean error, R response) {
+            this.error = error;
+            this.response = response;
+            this.exception = null;
+        }
+
+        Tolerance(boolean error, JresErrorResponseException exception) {
+            this.error = error;
+            this.exception = exception;
+            this.response = null;
+        }
+
+        /**
+         * @return whether or not an error response was captured: <code>true</code> {@link #getError()} is not null,
+         * <code>false</code> {@link #getResponse()} is not null
+         */
+        public boolean isError() {
+            return error;
+        }
+
+        /**
+         * @return captured {@link JresErrorResponseException}, if the request resulted in an error response
+         */
+        public JresErrorResponseException getError() {
+            return exception;
+        }
+
+        /**
+         * @return captured {@link JresResponse}, if the request was ok
+         */
+        public R getResponse() {
+            return response;
+        }
+
+    }
 }
 
