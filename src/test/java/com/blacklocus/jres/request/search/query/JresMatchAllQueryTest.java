c77b3a79326165266b1ed94f8d69e56633e83abd
Jason Dunkelberger (dirkraft) <dirkraft@gmail.com>
2013-12-13 09:15:29 -0600

    flesh out match_all test

--- src/test/java/com/blacklocus/jres/request/search/query/JresMatchAllQueryTest.java
+++ src/test/java/com/blacklocus/jres/request/search/query/JresMatchAllQueryTest.java
@@ -1,7 +1,17 @@
 package com.blacklocus.jres.request.search.query;
 
 import com.blacklocus.jres.BaseJresTest;
+import com.blacklocus.jres.model.search.Hit;
+import com.blacklocus.jres.request.index.JresIndexDocument;
+import com.blacklocus.jres.request.index.JresRefresh;
+import com.blacklocus.jres.request.search.JresSearch;
+import com.blacklocus.jres.request.search.JresSearchBody;
+import com.blacklocus.jres.response.search.JresSearchReply;
+import com.google.common.collect.Sets;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Set;
 
 /**
  * @author Jason Dunkelberger (dirkraft)
@@ -10,6 +20,24 @@ public class JresMatchAllQueryTest extends BaseJresTest {
 
     @Test
     public void test() {
-        // TODO
+        String index = "JresMatchAllQueryTest.test".toLowerCase();
+        String type = "test";
+
+        jres.quest(new JresIndexDocument(index, type, "1", new Document()));
+        jres.quest(new JresIndexDocument(index, type, "2", new Document()));
+        jres.quest(new JresIndexDocument(index, type, "3", new Document()));
+        jres.quest(new JresRefresh(index));
+
+        JresSearchReply searchReply = jres.quest(new JresSearch(index, type, new JresSearchBody().query(new JresMatchAllQuery())));
+        Assert.assertEquals(new Integer(3), searchReply.getHits().getTotal());
+        Set<String> ids = Sets.newHashSet("1", "2", "3");
+        for (Hit hit : searchReply.getHits().getHits()) {
+            ids.remove(hit.getId());
+        }
+        Assert.assertEquals("All records should have been returned", 0, ids.size());
+    }
+
+    static class Document {
+        public Integer the_answer = 42;
     }
 }
